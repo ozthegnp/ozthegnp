428e25a87083c6c81668c44aee7ca2406da60ceb
ozthgnp <oscaremateo@gmail.com>
2017-10-06 15:08:20 +0200

    armstrong working

--- week-01/day-5/armstrong/main.c
+++ week-01/day-5/armstrong/main.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 int main()
 {
-    int a = 371;
+    int a = 0;
+    printf("\n === Armstrong number checker ===\n\nPlease enter a number:");
+    scanf("%d", &a);
     int exp = 1;
-    int counter = a;
-
-    while(counter/10 != 0){
-        counter /= 10;
+    int digitcounter = a;
+    //digit counter
+    while(digitcounter/10 != 0){
+        digitcounter /= 10;
         exp++;
     }
-
+    //power adder
     int value = 0;
     int k = 1;
     int total = 0;
 
         for(int i = 1;i <= exp; i++){
             value = ((a % (k*10) - (a % k))/k );
-            printf("%d",value);
             k *= 10;
-
-                int result = value;
-                for (int i = 1; i <exp; i++){
-                    result = result*value;
-                }
-
-            printf("%d ", result);
-
-            total += value;
+                    int result = value;//power calculator
+                    for (int j = 1; j <exp; j++){
+                            result = result*value;
+                        }
+            total += result;
         }
 
-    printf("\n %d", total);
+    if(total == a) {
+            printf("%d is an Armstrong Number! Good job!\n", a);
+    } else {
+                    printf("%d is NOT an Armstrong Number! :( Try again\n", a);
+    }
 
     return 0;
 }
