c62cb64af411d01c344656781dac13f6487ac368
ozthgnp <oscaremateo@gmail.com>
2017-10-06 09:55:32 +0200

    added ascii program

---
+++ week-01/day-5/ascii/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    for(int d = 33; d <127; d++)
+    printf("ASCII value of character %c = %d\n", d, d);
+    return 0;
+}
+//create a program to print ASCII values of all characters.
+/*your outpu should be something like this:
+ASCII value of character 8 = 56
+ASCII value of character 9 = 57
+ASCII value of character : = 58
+ASCII value of character ; = 59
+ASCII value of character < = 60
+ASCII value of character = = 61
+ASCII value of character > = 62
+ASCII value of character ? = 63
+ASCII value of character @ = 64
+ASCII value of character A = 65
+ASCII value of character B = 66
+*/
