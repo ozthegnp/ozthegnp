abbf963096d83a020aef51b3ca5ef34c26212d2b
ozthgnp <oscaremateo@gmail.com>
2017-10-11 13:36:31 +0200

    improved directories

--- week-01/day-3/01.c
+++ week-01/day-3/01.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int a = 3;
 	a = a + 10;
 	printf(" %d \n", a);
--- week-01/day-3/02.c
+++ week-01/day-3/02.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int b = 100;
 	b = b - 7;
 	printf(" %d \n", b);
