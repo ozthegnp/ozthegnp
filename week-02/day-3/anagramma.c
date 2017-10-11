9875f36353ead3b115964eec9b3cb26fc5db16e5
ozthgnp <oscaremateo@gmail.com>
2017-10-11 16:58:10 +0200

    imporved anagram

--- week-02/day-3/anagramma.c
+++ week-02/day-3/anagramma.c
@@ -2,14 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void anagrammafinder(char anag1[], char anag2[]);
-
-
 void main ()
 {
-    char word1[] = "go34d";
+    char word1[] = "god";
     char word2[] = "dog";
-
 
     for(int i = 0; i < sizeof(word1); i++) {
        for(int j = 0; j < sizeof(word2);j++) {
@@ -27,36 +23,9 @@ void main ()
             }
        }
     }
-    printf("anfchk %d\n", ang_check);
+    if(ang_check == 0)
+        printf("It is an anagram!");
+    else
+        printf("Not an anagram!");
 
-
-    /*int result = 0;
-    for(int j = 0; j < 6; j++){
-    result += word1[j];
-    printf("%d", result);
     }
-
-    printf("%d\n", sizeof(word1));*/
-
-
-   /* for (int i = 0; i < sizeof(word1); i++) {
-        for(int j = 0; j < sizeof(word2); j++){
-           if(word1[i] == word2[j]){
-                for( int k = i; k < sizeof(word1); k++){
-                    word1[k] = word1[k+1];
-                }
-                for( int l = i; l < sizeof(word1); l++){
-                    word2[l] = word2[l+1];
-                }
-           }
-        }
-    }
-    puts(word1);
-    puts(word2);*/
-
-}
-
-void anagrammafinder(char anag1[], char anag2[])
-{
-
-}
