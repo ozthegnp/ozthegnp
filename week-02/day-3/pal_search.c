bdf2554782c460927777ce84b6b27f6e19eb7701
ozthgnp <oscaremateo@gmail.com>
2017-10-12 11:53:05 +0200

    palsearch

---
+++ week-02/day-3/pal_search.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int palsearchf(char word[]);
+
+void main()
+{
+    char word[]="";
+    gets(word);
+
+    for(int i = 3; i<= strlen(word); i++){
+        char subword[i];
+            for(int j = 0; j < strlen(word) - i ; j++){
+                    for(int k = 0; k < i;k++){
+                        subword[k] = word [j];
+                        puts(subword);
+                    }
+            }
+
+    }
+
+     if (palsearchf(word))
+        printf("Yes");
+     else
+        printf("No");
+}
+
+int palsearchf(char word[])
+{
+    int check = 0;
+    for(int i = 0; i < strlen(word); i++){
+            if(word[i] == word[strlen(word) - 1 - i]){
+                check++;
+            }
+    }
+    if(check == strlen(word))
+        return 1;
+    else
+        return 0;
+}
