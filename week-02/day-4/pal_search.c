bc977049d6b1b5f364abfe87bd2454e8a6c09bd3
ozthgnp <oscaremateo@gmail.com>
2017-10-13 11:20:49 +0200

    improved pal search

--- week-02/day-4/pal_search.c
+++ week-02/day-4/pal_search.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int palsearchf(char word[]);
+
 
 void main()
 {
-    char word[]="lolol";
-    for(int i = 3; i<= strlen(word); i++){
-        char subword[i];
-            for(int j = 0; j <= strlen(word)-i; j++){
-                    for(int k = 0; k < i;k++){
-                        subword[k] = word[k+j];
+    char word[] = "alukikulni";
+
+    int a = 5;
+    for (int i = 0; i <= strlen(word)-a; i++){
+        int check = 0;
+        for (int z = 0; z < a\2 ;z++) {
+            for(int j = i; j < i+a/2; j++) {
+                        printf("----i =%d  j = %d /%d", i, j, a + i - 1-f);
+                        printf("%c", word[j]);
+                        printf("%c", word[a + i - 1-f]);
+                        printf("\n");
+                        if(word[j] == word[a + i - 1-f]) {
+                            check++;
+                        }
+            }
+        }
+        printf("\n");
+        if(check == a/2){
+                    for (int k = i; k < i+a; k++){
+                        printf("%c", word[k]);
                     }
-                    puts(subword);
-                    printf("%d\n", palsearchf(subword));
-            }
+                    printf("\n");
+
+        }
 
     }
-     if (palsearchf(word))
-        printf("Yes");
-     else
-        printf("No");
-}
-
-int palsearchf(char word[])
-{
-    int check = 0;
-    for(int i = 0; i < strlen(word); i++){
-            if(word[i] == word[strlen(word) - 1 - i]){
-                check++;
-            }
-    }
-    if(check == strlen(word))
-        return 1;
-    else
-        return 0;
 }
