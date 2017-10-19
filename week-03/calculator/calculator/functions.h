f4c51b76488c447facea0823a5e112870e345d45
ozthgnp <oscaremateo@gmail.com>
2017-10-19 17:30:08 +0200

    calculator save

--- week-03/calculator/calculator/functions.h
+++ week-03/calculator/calculator/functions.h
@@ -3,52 +3,59 @@ float addition(float add1, float add2)
 {
     return add1 + add2;
 }
+
 float subtraction(float sub1, float sub2 )
 {
     return sub1 - sub2;
 }
+
 float multiplication(float mul1, float mul2 )
 {
     return mul1 * mul2;
 }
+
 float division(float div1, float div2 )
 {
     return div1 / div2;
 
 }
+
 int modulo(int mod1, int mod2 )
 {
     return mod1 % mod2;
-
 }
+
 int power(int pow1, int pow2 )
 {
     return pow(pow1, pow2);
 }
+
 int root(int r, int n)
 {
     return pow(r, 1./n);
 }
+
 float logarithm(float log1, float log2 )
 {
     return log(log2)/log(log1);
 }
+
 int binto(char *binto1, int binto2)
 {
         return strtol(binto1, NULL, 2);
 }
+
 int hexto(char *hex1, int hex2)
 {
         return strtol(hex1, NULL, 16);
 }
-int dectobin(int dec)
+
+long long int dectobin(long long int dec)
 {
-    if (dec == 0)
-    {
+    if (dec == 0) {
         return 0;
     }
-    else
-    {
+    else {
         return (dec % 2 + 10 * dectobin(dec / 2));
     }
 }
--- week-03/calculator/calculator/main.c
+++ week-03/calculator/calculator/main.c
@@ -3,19 +3,26 @@
 #include <math.h>
 #include "functions.h"
 
+void clrscr();
+
 void help();
 
 void set_cursor_pos(int x, int y);
-void clrscr();
+
 int space_counter(char word[]);
-int isitdigit(char *str);
+int isitdigit(char str[]);
+int bincheck(char num[]);
+int isithex(char str[]);
+
 void errormsg();
 
 char command[256];
 char *strx;
 char *stry;
 char *function;
-long x, y;
+char *ptr;
+float x = 0;
+float y = 0;
 int line_count = 1;
 
 int main()
@@ -29,13 +36,14 @@ int main()
 
         int space_count = space_counter(command);
 
-        if(strcmp(command, "clear") == 0){
+        if(strcmp(command, "clear") == 0) {
             clrscr();
             line_count = 0;
-        }else if(strcmp(command, "exit") == 0){
+        }else if(strcmp(command, "exit") == 0) {
             break;
-        }else if(strcmp(command, "help") == 0){
+        }else if(strcmp(command, "help") == 0) {
             help();
+            line_count = 0;
         }else if (space_count == 2){
 
             set_cursor_pos(strlen(command) + 21, line_count);
@@ -43,52 +51,71 @@ int main()
             strx = strtok(command, " ");
             function = strtok(NULL, " ");
             stry = strtok(NULL, " ");
+
+            printf("%s, %s",strx, stry);
+
             x = strtol(strx, NULL, 10);
             y = strtol(stry, NULL, 10);
 
                     if(isitdigit(stry) == 1 && strcmp(function, "hexto") == 0) {
+
                                     if(y == 10)
                                         printf("= %d\n", hexto(strx, y));
                                     else if (y == 2)
-                                        printf("= %d\n", dectobin(hexto(strx, y)));
+                                        printf("= %li\n", dectobin(hexto(strx, y)));
                                     else
                                       printf("Error. Type '2' or '10' after 'hexto'.\n");
+
                     } else if(isitdigit(stry) == 1 && isitdigit(strx) == 1) {
+
                             if(strcmp(function, "+") == 0){
-                                printf("= %.2f\n", addition(x, y));
+                                    printf("= %.2f\n", addition(x, y));
+
                             } else if(strcmp(function, "-") == 0){
-                                printf("= %.2f\n", subtraction(x, y));
+                                    printf("= %.2f\n", subtraction(x, y));
+
                             } else if(strcmp(function, "*") == 0){
-                                printf("= %.2f\n", multiplication(x, y));
-                            } else if(strcmp(function, "/") == 0){
+                                    printf("= %.2f\n", multiplication(x, y));
+
+                            } else if(strcmp(function, "/") == 0) {
                                 printf("= %.2f\n", division(x, y));
                                     if(y == 0){
                                         printf(" Oh-oh! you divided by zero! Don't do that.\n");
                                          line_count++;
                                     }
-                            } else if(strcmp(function, "%") == 0){
-                                printf("= %d\n", modulo(x, y));
-                            } else if(strcmp(function, "^") == 0){
-                                printf("= %d\n", power(x, y));
-                            } else if(strcmp(function, "<") == 0){
-                                printf("~= %d\n", root(x, y));
-                            } else if(strcmp(function, "log") == 0){
-                                printf("= %.1f\n", logarithm(x, y));
-                            } else if(strcmp(function, "binto") == 0){
-                                    if(y == 10)
-                                        printf("= %d\n", binto(strx, y));
-                                    else if (y == 16)
-                                        printf("= %x\n", binto(strx, y));
-                                    else
-                                      printf("Error. Type '10' or '16' after 'binto'.\n");
+                            } else if(strcmp(function, "%") == 0) {
+                                    printf("= %d\n", modulo(x, y));
 
-                            } else if(strcmp(function, "decto") == 0){
+                            } else if(strcmp(function, "^") == 0) {
+                                    printf("= %d\n", power(x, y));
+
+                            } else if(strcmp(function, "<") == 0) {
+                                    printf("~= %d\n", root(x, y));
+
+                            } else if(strcmp(function, "log") == 0) {
+                                    printf("= %.1f\n", logarithm(x, y));
+
+                            } else if(strcmp(function, "binto") == 0) {
+                                    if(bincheck(strx) == 0) {
+                                            if(y == 10)
+                                                printf("= %d\n", binto(strx, y));
+                                            else if (y == 16)
+                                                printf("= %x\n", binto(strx, y));
+                                            else
+                                                printf("Error. Type '10' or '16' after 'binto'.\n");
+                                    } else
+                                        printf("Error. '%d' is not binary\n", x);
+
+                            } else if(strcmp(function, "decto") == 0) {
+
                                     if(y == 2)
-                                        printf("= %d\n", dectobin(x));
+                                        printf("= %li\n", dectobin(x));
+
                                     else if (y == 16)
                                         printf("= %x\n", x);
+
                                     else
-                                      printf("Error. Type '2' or '16' after 'decto'.");
+                                      printf("Error. Type '2' or '16' after 'decto'.\n");
 
                             } else
                                 errormsg();
@@ -107,28 +134,28 @@ int main()
 void help()
 {
 
-    printf("\n\n    	CLI Calculator\n");
-    printf("====================================\n");
-    printf("usage: [number] [operation] [number]\n");
-    printf("Commands:\n\n");
-    printf(" +	addition\n");
-    printf(" -	subtraction\n");
-    printf(" *	multiplication\n");
-    printf(" /	division\n");
-    printf(" %	division with remainder\n");
-    printf(" ^	power\n");
-    printf(" <	square root\n");
-    printf(" log	logarithm\n");
-    printf(" binto	binary to hex or dec\n");
-    printf(" hexto	hexadecimal to bin or dec\n");
-    printf(" decto	decimal to bin or hex\n");
-    printf(" ====================================\n");
-    printf(" exit	exiting from the program\n");
-    printf(" clear	clear the screen\n");
-    printf(" help	print usage\n");
-    printf(" ====================================\n");
-    printf(" Hit enter to start!\n");
-    printf(" ====================================\n");
+    printf("\n\n    	CLI Calculator\n"
+    "====================================\n"
+    "usage: [number] [operation] [number]\n"
+    "Commands:\n\n"
+    " +	addition\n"
+    " -	subtraction\n"
+    " *	multiplication\n"
+    " /	division\n"
+    " %	division with remainder\n"
+    " ^	power\n"
+    " <	square root\n"
+    " log	logarithm\n"
+    " binto	binary to hex or dec\n"
+    " hexto	hexadecimal to bin or dec\n"
+    " decto	decimal to bin or hex\n"
+    " ====================================\n"
+    " exit	exiting from the program\n"
+    " clear	clear the screen\n"
+    " help	print usage\n"
+    " ====================================\n"
+    " Hit enter to start!\n"
+    " ====================================\n");
     getchar();
     clrscr();
 }
@@ -151,19 +178,44 @@ int space_counter(char word[])
     }
     return counter;
 }
-int isitdigit(char *str)
+int isitdigit(char str[])
 {
+    int result = 1;
+
     for (int i=0;i<strlen(str); i++) {
-        if (!isdigit(str[i]))
-        {
-            return 0;
-            break;
-        } else
-        return 1;
+        if (str[i] < 45 || str[i] > 57 || str[i] == 47){
+            result = 0;
+        }
     }
+
+    return result;
 }
 void errormsg()
 {
     printf("Error. Type 'help' if confused.\n");
 
+}
+int bincheck(char num[])
+{
+    int bin_count = 0;
+    for (int i = 0; i < strlen(num); i++){
+
+        if(num[i] == '0'|| num[i] == '1'){
+            bin_count++;
+        }
+    }
+    return strlen(num)-bin_count;
+
+}
+int isithex(char str[])
+{
+    int result = 1;
+
+    for (int i=0;i<strlen(str); i++) {
+        if (str[i] < 45 || str[i] > 57 || str[i] == 47){
+            result = 0;
+        }
+    }
+
+    return result;
 }
