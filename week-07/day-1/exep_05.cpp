c1c1158617ba7f4abb0f01a68a6a9070637bc9ee
ozthgnp <oscaremateo@gmail.com>
2017-11-20 13:20:32 +0100

    added 05

---
+++ week-07/day-1/exep_05.cpp
@@ -0,0 +1,32 @@
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+// Create a function which throws an char
+// In the main(), put that function in a try block
+// Also in the main() catch, what your function throws
+
+void null_check(int b){
+    if(b == 0)
+        throw 'a';
+}
+
+int main() {
+
+    try{
+
+        int a = 1;
+        int b = 0;
+
+        null_check(b);
+
+        cout << a/b << endl;
+
+    } catch(char a){
+        cout << a << endl;
+    }
+    return 0;
+}
