d7043a53e736306643cdce5cb07d857b103fca3f
Oscar <oscaremateo@gmail.com>
2017-11-24 14:44:45 +0100

    troublshooting finished

--- week-07/day-4/Temperature/main.cpp
+++ week-07/day-4/Temperature/main.cpp
@@ -1,235 +1,135 @@
 #include <iostream>
-
 #include <string>
-
 #include <vector>
-
 #include <windows.h>
-
 #include <conio.h>
-
 #include <stdio.h>
-
 #include "data.h"
-
-//#include <
-
-
 
 #include "SerialPortWrapper.h"
 
-
-
 using namespace std;
-
-
 
 void WelcomeScreen();
 
-
-
 Data data_checker(string user_input);
 
-
-
 int stoi(string s);
-
-
 
 int main()
 
 {
 
     Data temp;
-
     vector<Data> data_vector;
 
 
 
-    if(data_checker("2016.08.09 23:34:43 435").year < 0){
-
-        cout << "Invalid" << endl;
-
-    };
-
-
-
-
-
     vector<string> ports = SerialPortWrapper::listAvailablePorts();
-
     cout << "Number of found serial ports: " << ports.size() << endl;
 
-
-
     unsigned int i = 0;
-
     for (i = 0; i < ports.size(); i++) {
-
         cout << "\tPort name: " << ports.at(i) << endl;
-
     }
-
-    // connection
-
-
-
     SerialPortWrapper *serial = new SerialPortWrapper(ports.at(0), 115200);
-
-    //serial->openPort();
-
-    string line;
-
-
-
-    // dinamikus vektortoltes for ciklus helyett pushback-kel
-
-    // do while hatultesztelossel az ujabb 's' parancsig futtat
 
     string input;
 
-    //bool portflag = false;
-
     while(1){
 
-
-
-        //if(portflag){
-
-            //serial->openPort();
-
-        //}
-
         cout << "Please add input" << endl << endl;
-
         cin >> input;
 
-
-
         if(input == "h"){
-
             WelcomeScreen();
-
-        }
-
-        else if(input == "o"){
-
+        } else if(input == "o") {
             serial->openPort();
-
             cout << "Port opened" << endl << endl;
-
-            }
-
-        else if(input == "s"){
-
+        } else if(input == "s"){
             cout << "Logging started\n";
-
             while(1){
+                string line = "";
 
-            if(kbhit()){
+                if(kbhit()){
+                    cout << "Logging stopped" << endl << endl;
+                    break;
+                }
 
-                cout << "Logging stopped" << endl << endl;
+                serial->readLineFromPort(&line);
 
-                break;
+                if (line.length() > 0){
 
+                    try{
+                    data_vector.push_back(data_checker(line));
+                    cout << "record logged" << endl;
+
+                    }catch(const char* error){
+                    //cout << "there is a problem: " << error << endl;
+                    }
+                }
             }
-
-            serial->readLineFromPort(&line);
-
-            if (line.length() > 0){
-
-            cout << line << endl;
-
-            }
-
-        }
-
-
-
-        }
-
-        else if(input == "c"){
+        } else if(input == "c"){
 
             serial->closePort();
 
             cout << "Port closed" << endl;
 
-        }
+        } else if(input == "l"){
 
-        else if(input == "l"){
+            int sum = 0;
+            int temp_max = 0;
 
-            //todo
+            for(unsigned int i = 0; i < data_vector.size(); ++i){
+                    cout << data_vector[i].year << ".";
+                    cout << data_vector[i].month << ".";
+                    cout << data_vector[i].day << " ";
+                    cout << data_vector[i].hour << ":";
+                    cout << data_vector[i].minute << ":";
+                    cout << data_vector[i].second << " ";
+                    cout << data_vector[i].temperature << endl;
 
-        }
+                    sum += data_vector[i].temperature;
+                    if(data_vector[i].temperature > temp_max){
+                        temp_max = data_vector[i].temperature;
+                    }
+            }
 
-        else if(input == "e"){
-
+            cout << "Avarage temperature is: " << sum / data_vector.size() << endl;
+            cout << "Max temperature: " << temp_max << endl;
+        } else if(input == "e"){
             exit(0);
-
         }
-
     }
-
-
-
     return 0;
-
 }
-
-
 
 void WelcomeScreen(){
 
-
-
-    cout << "Temperature Logger Application\n";
-
-    cout << "==============================" << endl;
-
-    cout << "Commands: " << endl;
-
-    cout << "h  Show command list " << endl;
-
-    cout << "o  Open port " << endl;
-
-    cout << "s  Start logging / Stop logging" << endl;
-
-    cout << "c  Close port " << endl;
-
-    cout << "l  List after error handling " << endl;
-
-    cout << "e  Exit from the program " << endl;
-
-    cout << "==============================" << endl;
-
-    cout << endl;
+    cout << "Temperature Logger Application\n"
+     "==============================\n"
+     "Commands: \n"
+     "h  Show command list \n"
+     "o  Open port \n"
+     "s  Start logging / Stop logging\n"
+     "c  Close port \n"
+     "l  List after error handling \n"
+     "e  Exit from the program \n"
+     "==============================" << endl << endl;
 
 
 
 }
 
-int stoi(string str)
-
-{
-
+int stoi(string str){
     const char* s = str.c_str();
-
-
-
     long i;
-
     i = 0;
 
-    while(*s >= '0' && *s <= '9')
-
-    {
-
+    while(*s >= '0' && *s <= '9') {
         i = i * 10 + (*s - '0');
-
         s++;
-
     }
-
     return i;
 
 }
@@ -237,118 +137,74 @@ int stoi(string str)
 
 
 Data data_checker(string user_input){
-
     Data data;
-
-
-
-    try{
-
         size_t pos = user_input.find('.');//looks for char
 
 
 
         data.year = stoi(user_input.substr(0, pos));//fills up string until char
-
-        cout << data.year << endl;
-
         if(data.year > 2016){
-
-            throw 1;
-
+            throw "Invalid Year";
         }
 
-
-
         user_input.erase(0, pos + 1); // deletes everythin befor char in original string
-
         pos = user_input.find('.'); //looks for new char
-
-
-
-
         data.month = stoi(user_input.substr(0, pos));
 
-        cout << data.month << endl;
-
-
-
-        //task = user_input.substr(0, pos);//fills up task string with other info
-
-
+        //if(data.month > 12 || data.month < 1){
+          //  throw "Invalid Month";
+       // }
 
         user_input.erase(0, pos + 1); // deletes everythin befor char in original string
-
         pos = user_input.find(' '); //looks for new char
-
-
-
         data.day = stoi(user_input.substr(0, pos));
 
-        cout << data.day << endl;
+        if(data.day > 31 || data.day < 1){
+            throw "Invalid Day";
+        }
+        if(data.month == 2){
+            if((data.year % 4 != 0 && data.day > 28) || (data.year % 4 == 0 && data.day > 29)){
+                throw "February error";
 
+           }
+        }
+        if(data.month < 8 && data.month % 2 == 0 && data.day > 30){
+                throw "Month has only 30 days";
 
+        } else if(data.month > 7 && data.month %2 == 1 && data.day > 30){
+                throw "Month has only 30 days";
+        }
 
         user_input.erase(0, pos + 1); // deletes everythin befor char in original string
-
         pos = user_input.find(':'); //looks for new char
-
-
-
         data.hour = stoi(user_input.substr(0, pos));
 
-        cout << data.hour << endl;
-
-
+        if(data.hour > 24 || data.hour < 0){
+            throw "invalid hour";
+        }
 
         user_input.erase(0, pos + 1); // deletes everythin befor char in original string
-
         pos = user_input.find(':'); //looks for new char
-
-
-
         data.minute = stoi(user_input.substr(0, pos));
 
-        cout << data.minute << endl;
-
-
+        if(data.minute > 59 || data.minute < 0){
+            throw "invalid minute";
+        }
 
         user_input.erase(0, pos + 1); // deletes everythin befor char in original string
-
         pos = user_input.find(' '); //looks for new char
-
-
-
         data.second = stoi(user_input.substr(0, pos));
 
-        cout << data.second << endl;
-
-
+        if(data.second > 59 || data.second < 0){
+            throw "invalid second";
+        }
 
         user_input.erase(0, pos + 1); // deletes everythin befor char in original string
-
-
-
         data.temperature = stoi(user_input);
 
-        cout << data.temperature << endl;
-
-    } catch(int a){
-
-        data.year = -1;
-
-        return data;
-
-    }
-
-
-
-
-
-    //priority = user_input;
-
-
-
+        if(data.temperature > 1000 || data.temperature < -273){
+            throw "invalid temp";
+        }
     return data;
 
 }
