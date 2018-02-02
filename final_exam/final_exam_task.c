b0f13c69bda44749886e9df4bf72ee52209aa792
ozthgnp <oscaremateo@gmail.com>
2018-02-02 09:55:25 +0100

    add reverse str function

--- final_exam/final_exam_task.c
+++ final_exam/final_exam_task.c
@@ -10,6 +10,20 @@
  //prototyping function
 void reverse_string(char string[], int length);
 
-int main() {    
+int main() {
     return 0;
+}
+
+void reverse_string(char string[], int length)
+{
+    printf("Your string reversed is:\n");
+
+    for(int i = length - 1; i >= 0; i--){
+        printf("%c", string[i]);
+        //adding an extra \n to make the string look nice
+        if(i == 0){
+            printf("\n");
+        }
+    }
+
 }
