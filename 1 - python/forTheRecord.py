42348f3046881e3826909005134d1d8163f7e38c
ozthgnp <oscaremateo@gmail.com>
2019-05-01 18:30:17 +0200

    add letter grade function

--- 1 - python/forTheRecord.py
+++ 1 - python/forTheRecord.py
@@ -22,3 +22,38 @@ lloyd["quizzes"] = [88., 40., 94.]
 lloyd["tests"] = [75., 90.]
 
 students = [lloyd, alice, tyler]
+
+# Average function
+def average(numbers):
+  total = float(sum(numbers))
+  return total/len(numbers)
+
+# Print all student info
+def print_all_student_info():
+    for student in students:
+        print student["name"]
+        print student["homework"]
+        print student["quizzes"]
+        print student["tests"]
+
+#Get weighed average for each student
+def get_average(student):
+  homework = average(student["homework"])
+  quizzes = average(student["quizzes"])
+  tests = average(student["tests"])
+  return .1 * homework + .3 * quizzes + .6 * tests
+
+# Letter grade function
+def get_letter_grade(score):
+  if score > 89:
+    return "A"
+  elif score > 79:
+    return "B"
+  elif score > 69:
+    return "C"
+  elif score > 59:
+    return "D"
+  else:
+    return "F"
+
+print get_letter_grade(get_average(alice))
