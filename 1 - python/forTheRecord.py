735c59c9cd3e7b0c03810de3f33c19ad72049067
ozthgnp <oscaremateo@gmail.com>
2019-05-01 18:03:24 +0200

    init forTheRecord

---
+++ 1 - python/forTheRecord.py
@@ -0,0 +1,24 @@
+lloyd = {
+  "name": "Lloyd",
+  "homework": [],
+  "quizzes": [],
+  "tests": []
+}
+alice = {
+  "name": "Alice",
+  "homework": [100.0, 92.0, 98.0, 100.0],
+  "quizzes": [82.0, 83.0, 91.0],
+  "tests": [89.0, 97.0]
+}
+tyler = {
+  "name": "Tyler",
+  "homework": [0.0, 87.0, 75.0, 22.0],
+  "quizzes": [0.0, 75.0, 78.0],
+  "tests": [100.0, 100.0]
+}
+
+lloyd["homework"] = [90., 97., 75., 92.]
+lloyd["quizzes"] = [88., 40., 94.]
+lloyd["tests"] = [75., 90.]
+
+students = [lloyd, alice, tyler]
