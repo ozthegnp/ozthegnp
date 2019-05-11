8b52b541589f6a7173bbcf0f1c4d0f540e83a246
ozthgnp <oscaremateo@gmail.com>
2019-05-11 12:41:03 +0200

    update remove_duplicates with comments

--- 1 - python/remove_duplicates.py
+++ 1 - python/remove_duplicates.py
@@ -1,16 +1,19 @@
-def remove_duplicates(list):
+def remove_duplicates(input_list):
+  #init output list
   unique_list = []
 
-  for i in list:
-    found_in_list = False
+  #iterating through input list
+  for i in input_list:
+    found_in_list = False # setting flag, assuming,that the element did not occur yet
 
     for j in unique_list:
       if i == j:
-        found_in_list = True
+        found_in_list = True #if the element did occure already, setting flag to true
 
+    #in case it did not occur in the unique list, adding it to the unique list.
     if found_in_list == False:
       unique_list.append(i)
 
   return unique_list
 
-print remove_duplicates([1, 1, 2, 2])
+print remove_duplicates(['hello', 'hello', 2, 2])
