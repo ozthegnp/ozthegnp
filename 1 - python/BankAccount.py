02ab7005ea1bd10e207b52ce239b79ee1a0dd722
ozthgnp <oscaremateo@gmail.com>
2019-05-12 17:44:08 +0200

    add withdraw method and tests

--- 1 - python/BankAccount.py
+++ 1 - python/BankAccount.py
@@ -6,12 +6,13 @@ class BankAccount(object):
   def __repr__(self):
     return self.name + "'s balance: ${:.2f}".format(self.balance)
 
+  # SHOWBALANCE METHOD
   def show_balance(self):
     print self.name + \
   "'s balance: ${:.2f}".format(self.balance)
 
+  # DEPOSIT METHOD
   def deposit(self, amount):
-
     if amount <= 0:
       print "Invalid deposit"
       return
@@ -20,6 +21,24 @@ class BankAccount(object):
       self.balance += amount
       self.show_balance()
 
-hello = BankAccount('Oscar')
-hello.show_balance()
-hello.deposit(2)
+  # WITHDRAW METHOD
+  def withdraw(self, amount):
+    if amount > self.balance:
+      print "Insufficient balance"
+      return
+    elif amount <= 0:
+      print "Invalid withdrawal"
+      return
+    else:
+      print "You are withdrawing ${:.2f}".format(amount)
+      self.balance -= amount
+      self.show_balance()
+
+
+#Testing functionalities
+my_account = BankAccount('Oscar')
+print my_account
+my_account.show_balance()
+my_account.deposit(2000)
+my_account.withdraw(1000)
+print my_account
